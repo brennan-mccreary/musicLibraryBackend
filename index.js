dd938deb7f123c39e01787a22b9cba906a05fb39
Brennan McCreary <brennanmccreary@gmail.com>
2022-01-18 20:00:08 -0500

    Add imports and request listener and handler.

---
+++ index.js
@@ -0,0 +1,61 @@
+//import and assign express
+const express = require('express');
+const app = express();
+
+//CORS future implementation line 
+//
+
+//Middleware future implementation line
+//
+
+//app.use(corse());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Import repository wrapper
+const repoContext = require('./repository/repository-wrapper.js');
+
+//Listener
+const PORT = 3000; //define port value
+app.listen(PORT, (PORT) => {
+    console.log(`Server started. Listening on port ${PORT}`);
+});
+
+//Request handlers
+//
+//Handle GET REQ -- All Songs
+app.get('/api/songs', (req, res) => {
+    const songs = repoContext.songs.findAllSongs();
+    return res.send(songs);
+});
+
+//Handle GET REQ -- Song by ID
+app.get('/api/songs/:id', (req, res) => {
+    const id = req.params.id;
+    const song = repoContext.songs.findSongById(id);
+    return res.send(song);
+});
+
+//Handle POST REQ -- New Song (Validate)
+app.post('/api/songs',/* [validateSong],*/(req, res) => {
+    const newSong = req.body;
+    const addedSong = repoContext.songs.createSong(newSong);
+    return res.send(addedSong);
+});
+
+//Handle PUT REQ -- Edit Song by ID (Validate)
+app.put('/api/songs/:id',/* [validateSong],*/(req, res) => {
+    const id = req.params.id;
+    const songUpdatedProperties = req.body;
+    const updatedSong = repoContext.songs.updateSong(id, songUpdatedProperties);
+    return res.send(updatedSong);
+});
+
+//Handle DELETE REQ -- Delete Song by ID
+app.delete('/api/songs/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedDataSet = repoContext.songs.deleteSong(id);
+    return res.send(updatedDataSet);
+});
+
+
